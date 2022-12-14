39b75a746990f61e7e3266579217af024f7493fb
LLBF <ll.balsecawork@gmail.com>
2022-12-14 20:00:16 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+# cuál nombre de app??, y autores??
+setup(
+    name= 'datascienceproyect',
+    version= '0.0.1',
+    license= 'MIT',
+    long_description= 'Librería para limpieza y visualización de datos, y Machine learning',
+    author= 'Varios',
+    url= 'https://github.com/LLBF/Proyecto_libreria_DS'
+
+)
